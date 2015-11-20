cc929ed86823933631e6659ce91c007fbe76beeb
acclaris <chaithanya.pejjai@accenture.com>
2015-11-20 17:43:05 +0530

    Transaction implemented AOP- logger Implemented

--- hospital-app/src/main/java/com/acclaris/hospital/controller/HospitalUserSearchController.java
+++ hospital-app/src/main/java/com/acclaris/hospital/controller/HospitalUserSearchController.java
@@ -2,6 +2,7 @@ package com.acclaris.hospital.controller;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -18,6 +19,10 @@ public class HospitalUserSearchController {
 @Autowired
 HospitalUserSearchService hospitalUserSearchservice;
 
+
+private static final Logger logger = Logger.getLogger(HospitalUserSearchController.class);
+
+
 /**
  * @return
  * @throws Exception
@@ -26,11 +31,18 @@ HospitalUserSearchService hospitalUserSearchservice;
 public String getTestIndexPage(String searchString, String searchType, ModelMap model) throws Exception{	
 	List<HospitalUser>  userList = hospitalUserSearchservice.getUserList("u%%Er*", "EMAIL_ID");
 	model.addAttribute("userList", userList);
+	
+	if(logger.isDebugEnabled()){
+		logger.debug("getWelcome is executed!");
+	}
 	for (HospitalUser hospitalUser : userList) {
 		System.out.println("Name  "+ hospitalUser.getFirstName());
 	}
 	return "index";			
 }
+
+
+
 
 
 }
